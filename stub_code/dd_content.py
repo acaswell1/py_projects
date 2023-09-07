72dc1f099ceef82a231b6203f14015ba03680efe
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-09-07 21:36:18 +0000

    stub code added

---
+++ stub_code/dd_content.py
@@ -0,0 +1,15 @@
+""" Test """
+def get_random_quote():
+    """ Test """
+
+def get_weather_forcast():
+    """ Test """
+
+def get_twitter_trends():
+    """ Test """
+
+def get_wikipedia_article():
+    """ Test """
+
+if __name__ == '__main__':
+    pass #test code
---
+++ stub_code/dd_email.py
@@ -0,0 +1,21 @@
+"""
+    TEST IMPLEMENTATION
+
+"""
+
+
+class DailyDigestEmail:
+    """ Test """
+
+    def __init__(self):
+        pass
+
+    def send_email(self):
+        """ Test """
+
+    def format_message(self):
+        """ Test """
+
+
+if __name__ == '__main__':
+    pass  # test code
